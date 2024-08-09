25b84c635a1426e95e6fe4e463c906b2f0d8ef92
19kirti <aggarwalkirti13579@gmail.com>
2024-08-09 15:45:37 +0530

    loadingBarAdded

--- src/News.jsx
+++ src/News.jsx
@@ -92,7 +92,7 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h2>NEWS.next - Top {this.props.category} Headlines</h2>
+        <h2 style={{marginTop: '90px', marginBottom: '40px'}}>NEWS.next - Top {this.props.category} Headlines</h2>
 
         {this.state.loading && <Loading/>}
 
